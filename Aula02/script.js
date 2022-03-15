46f63c1dcabaedb004f6bd02278ac1cec6c58ed4
rfluan <luanramosferr@outlook.com>
2022-03-15 20:47:40 -0300

    Valor em Bitcoin

--- Aula02/script.js
+++ Aula02/script.js
@@ -3,9 +3,13 @@ function ConverterReal() {
     var valor = valorElemento.value
     var valorEmDolarNumerico = parseFloat(valor)
     var valorEmReal = valorEmDolarNumerico * 5.16
+    var valorEmBitcoin = valorEmDolarNumerico * 0.000025
 
     var elementoValorConvertido = document.getElementById("valorConvertido")
-    var valorConvertido = `O resultado em real é R$ ${valorEmReal}!`
+    var valorConvertido = `O resultado em real é R$ ${valorEmReal}! e <br> em Bitcoin é ${valorEmBitcoin}`
+
+    console.log(valorEmBitcoin)
+
     elementoValorConvertido.innerHTML = valorConvertido
 }
 
@@ -24,6 +28,6 @@ function ConverterEuro() {
 Desafios desta aula!
 [ok] Adicionar outras moedas para converter.
 [] Conversor de quilômetros para anos luz e verificar o tempo que demora para ir de uma estrela para outra.
-[] Conversor de temperaturas entre farenheit, kelvin e celcius.
-[] Adicionar uma linha ao projeto desenvolvido para que apareça o valor em bitcoin.
+[ok] Conversor de temperaturas entre farenheit, kelvin e celcius.
+[ok] Adicionar uma linha ao projeto desenvolvido para que apareça o valor em bitcoin.
 */
