7840d2fe616e7dc7bae65e162f135436e7c931e1
rfluan <luanramosferr@outlook.com>
2022-03-15 20:37:54 -0300

    converter em euro

--- Aula02/script.js
+++ Aula02/script.js
@@ -1,12 +1,29 @@
-function Converter() {
+function ConverterReal() {
     var valorElemento = document.getElementById("valor")
     var valor = valorElemento.value
     var valorEmDolarNumerico = parseFloat(valor)
-
-    var valorEmReal = valorEmDolarNumerico * 5
-    console.log(valorEmReal)
+    var valorEmReal = valorEmDolarNumerico * 5.16
 
     var elementoValorConvertido = document.getElementById("valorConvertido")
-    var valorConvertido = `O resultado em real R$ ${valorEmReal}!`
+    var valorConvertido = `O resultado em real é R$ ${valorEmReal}!`
     elementoValorConvertido.innerHTML = valorConvertido
 }
+
+function ConverterEuro() {
+    var valorElemento = document.getElementById("valor")
+    var valor = valorElemento.value
+    var valorEmDolarNumerico = parseFloat(valor)
+    var valorEmEuro = valorEmDolarNumerico * 0.91
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+    var valorConvertido = `O resultado em Euro é € ${valorEmEuro}!`
+    elementoValorConvertido.innerHTML = valorConvertido
+}
+
+/*
+Desafios desta aula!
+[ok] Adicionar outras moedas para converter.
+[] Conversor de quilômetros para anos luz e verificar o tempo que demora para ir de uma estrela para outra.
+[] Conversor de temperaturas entre farenheit, kelvin e celcius.
+[] Adicionar uma linha ao projeto desenvolvido para que apareça o valor em bitcoin.
+*/
