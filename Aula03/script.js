ec3551be7149fb0be788fa3587572ed737f0b730
rfluan <luanramosferr@outlook.com>
2022-03-16 20:57:57 -0300

    Finalizado Aula 03

--- Aula03/script.js
+++ Aula03/script.js
@@ -1,4 +1,5 @@
-var numeroSecreto = parseInt(Math.random() * 11);
+var numeroSecreto = parseInt(Math.random() * 21);
+var tentativas = 4;
 
 function Chutar() {
   var elementoResultado = document.getElementById("resultado");
@@ -6,39 +7,23 @@ function Chutar() {
 
   if (chute == numeroSecreto) {
     elementoResultado.innerHTML = "Parabéns, você acertou!";
-  } else if (chute > 10 || chute < 0) {
+  } else if (chute > 20 || chute < 0) {
     elementoResultado.innerHTML = "Adicione um número de 0 a 10!";
-  } else if (chute > numeroSecreto) {
-    elementoResultado.innerHTML =
-      "O número chutado é MAIOR que o número secreto!";
-  } else if (chute < numeroSecreto) {
-    elementoResultado.innerHTML =
-      "O número chutado é MENOR que o número secreto!";
-  } else {
-    elementoResultado.innerHTML = "Você errou!";
+  } else if (numeroSecreto > chute) {
+    elementoResultado.innerHTML = `Poxa, que pena! Você errou! O número secreto é maior que ${chute}. Você tem mais ${tentativas} tentativas.`;
+    tentativas--;
+  } else if (numeroSecreto < chute) {
+    elementoResultado.innerHTML = `Poxa, que pena! Você errou! O número secreto é menor que ${chute}. Você tem mais ${tentativas} tentativas.`;
+    tentativas--;
+  }
+  if (tentativas < 0) {
+    elementoResultado.innerHTML = `Número máximo de tentativas alcançado. Você perdeu! O número secreto era:  ${numeroSecreto} .`;
   }
 }
 
 /*
 Desafios desta aula!
-[] Adicionar um número de tentativas para a pessoa tentar acertar e imprimir a resposta no final.
+[ok] Adicionar um número de tentativas para a pessoa tentar acertar e imprimir a resposta no final.
 [ok] Quando a pessoa errar, deixar na mensagem se o número chutado é maior ou menor que o número secreto.
 [ok] Pesquisar e aprender a diferença entre == e ===
-
-if (chute == numeroSecreto) {
-    elementoResultado.innerHTML = "Parabéns, você acertou!";
-  } else if (chute > 10 || chute < 0) {
-    elementoResultado.innerHTML = "Adicione um número de 0 a 10!";
-  } else if (chute > numeroSecreto) {
-    elementoResultado.innerHTML =
-      "O número chutado é MAIOR que o número secreto!";
-  } else if (chute < numeroSecreto) {
-    elementoResultado.innerHTML =
-      "O número chutado é MENOR que o número secreto!";
-  } else {
-    elementoResultado.innerHTML = "Você errou!";
-  }
-}
-
-
 */
