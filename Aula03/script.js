1746c62d21cd63c2a3da19536b0b98c1de508941
rfluan <luanramosferr@outlook.com>
2022-03-16 20:35:25 -0300

    Update script.js

--- Aula03/script.js
+++ Aula03/script.js
@@ -8,6 +8,12 @@ function Chutar() {
     elementoResultado.innerHTML = "Parabéns, você acertou!";
   } else if (chute > 10 || chute < 0) {
     elementoResultado.innerHTML = "Adicione um número de 0 a 10!";
+  } else if (chute > numeroSecreto) {
+    elementoResultado.innerHTML =
+      "O número chutado é MAIOR que o número secreto!";
+  } else if (chute < numeroSecreto) {
+    elementoResultado.innerHTML =
+      "O número chutado é MENOR que o número secreto!";
   } else {
     elementoResultado.innerHTML = "Você errou!";
   }
@@ -16,6 +22,23 @@ function Chutar() {
 /*
 Desafios desta aula!
 [] Adicionar um número de tentativas para a pessoa tentar acertar e imprimir a resposta no final.
-[] Quando a pessoa errar, deixar na mensagem se o número chutado é maior ou menor que o número secreto.
-[] Pesquisar e aprender a diferença entre == e ===
+[ok] Quando a pessoa errar, deixar na mensagem se o número chutado é maior ou menor que o número secreto.
+[ok] Pesquisar e aprender a diferença entre == e ===
+
+if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "Parabéns, você acertou!";
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Adicione um número de 0 a 10!";
+  } else if (chute > numeroSecreto) {
+    elementoResultado.innerHTML =
+      "O número chutado é MAIOR que o número secreto!";
+  } else if (chute < numeroSecreto) {
+    elementoResultado.innerHTML =
+      "O número chutado é MENOR que o número secreto!";
+  } else {
+    elementoResultado.innerHTML = "Você errou!";
+  }
+}
+
+
 */
