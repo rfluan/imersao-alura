8086cc59e2e5a2096be6948c68bc0803e94b2dfc
rfluan <luanramosferr@outlook.com>
2022-03-18 12:02:35 -0300

    Update script.js

--- Aula05/script.js
+++ Aula05/script.js
@@ -1,63 +1,68 @@
-var listaAnime = [
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXhERtxmqWKErqEPFbIhL-0wfg2wLKHJfWJu6aJ3Rj3Q_lBDauTXqXUlcJ3A8GBKZDcwo&usqp=CAU",
-
-  "https://sm.ign.com/ign_br/tv/m/my-hero-ac/my-hero-academia_f9ae.jpg",
-  
-  "https://media.fstatic.com/XcbavtyoNtRzTHPYHEBzbJ24pvg=/290x478/smart/media/movies/covers/2015/04/akame-ga-kill_t101243.jpg"
-];
-
-//laço de repetição responsável por apresentar todos os livros add
-
-for (var i = 0; i < listaAnime.length; i++) {
-  document.write("<img src=" + listaAnime[i] + ">");
-}
-
-//funçao que adicionará as url's observando condições
-
-function incluirAnime() {
-  novoAnime = document.getElementById("novoAnime").value;
-
-  //condicional que testará se url já foi incluida anteriormente
-  if (listaAnime.indexOf(novoAnime) < 0) {
-    //fazendo o push caso a url seja nova
-    listaAnime.push(novoAnime);
-    document.body.innerHTML +=
-      "<img src=" + listaAnime[listaAnime.length - 1] + ">";
+function adicionarFilme() {
+  var filmeFavorito = document.getElementById("filme").value;
+  if (filmeFavorito.endsWith(".jpg")) {
+    listarFilmesNaTela(filmeFavorito);
   } else {
-    //alert na tela caso seja uma url já add
-    alert("Esse anime já existe no catálogo");
+    console.error("Endereço de filme inválido!");
   }
+  document.getElementById("filme").value = "";
 }
 
-
-
-
-
-
+function listarFilmesNaTela(filme) {
+  var elementoFilmeFavorito = `<img src= ${filme} >`;
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML =
+    elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+}
 
 /*
-
   Desafios desta aula!
-  
-[ok] Escolher o seu tema preferido para adaptar ao nosso código, ou seja, ao invés de filmes pode ser uma lista de animes, HQ's, cursos, capas de jogos...
-[] Tentar implementar outras versões da estrutura de repetição que fizemos com for, como por exemplo com foreach ou while
-[ok] Criar uma condição para não adicionar filmes repetidos, caso eles já tenham sido adicionados anteriormente
-[ok] Criar um campo e botão para adicionar a imagem pela tela, e não direto no código
 
+[] Criar um botão para remover um filme na tela
+[] Além de colocar a imagem do filme, também adicionar o nome por meio de outro input
+[] Guardar todos os filmes adicionados em uma lista/array e percorrer essa lista toda vez que quiser imprimir ou remover o filme
+[] Conversor de moedas: criar funções para cada tipo de conversão e chamá-las dependendo do que você quiser fazer, podendo colocar [] inputs ou botões e quando clicar chamar cada função converteDolar(), converteReal(), converteEuro() e converteBitcoin(), por exemplo
 
-  Conteúdo detalhado desta aula
-Criar uma primeira array de filmes usando a sintaxe [];
+  Conteúdo detalhado desta aula:
+Sintaxe e criação de funções no JavaScript;
+Integrando funções criadas no JavaScript com o HTML que está sendo exibido na tela;
+Condicionando a execução (ou "chamada") de uma função a um clique em um botão na tela;
+Usando o JavaScript para acessar o que está sendo exibido na tela e pegar valores digitados pelo usuário com getElementById() e .value;
+Passar informações que as funções precisam para funcionar, através dos parâmetros;
+Utilizar o .endsWith() para verificar se um texto termina com determinados caracteres;
+Ver mais um exemplo de reatribuição de variável para "limpar" o texto do campo com "".
+*/
+function adicionarFilme() {
+  var filmeFavorito = document.getElementById("filme").value;
+  if (filmeFavorito.endsWith(".jpg")) {
+    listarFilmesNaTela(filmeFavorito);
+  } else {
+    console.error("Endereço de filme inválido!");
+  }
+  document.getElementById("filme").value = "";
+}
 
-Utilizar o método filmes.push("Nome Do Filme") para inserir um novo elemento na lista (ou seja, um novo filme na array);
-Descobrir a quantidade de elementos em uma array com o método array.length;
+function listarFilmesNaTela(filme) {
+  var elementoFilmeFavorito = `<img src= ${filme} >`;
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML =
+    elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+}
 
-Selecionar elementos de uma array utilizando a sintaxe array[número], lembrando sempre que o primeiro índice começa com zero, ou seja, array[0] para o primeiro elemento;
+/*
+  Desafios desta aula!
 
-Utilizar a instrução for para iterar, ou seja, percorrer todos os elementos de uma array;
+[] Criar um botão para remover um filme na tela
+[] Além de colocar a imagem do filme, também adicionar o nome por meio de outro input
+[] Guardar todos os filmes adicionados em uma lista/array e percorrer essa lista toda vez que quiser imprimir ou remover o filme
+[] Conversor de moedas: criar funções para cada tipo de conversão e chamá-las dependendo do que você quiser fazer, podendo colocar [] inputs ou botões e quando clicar chamar cada função converteDolar(), converteReal(), converteEuro() e converteBitcoin(), por exemplo
 
-Criar uma array com imagens de pôsters de alguns filmes que gostamos;
-
-Montar a lógica do programa que vai iterar esta array de filmes e exibir cada um deles na tela, integrando o for do JavaScript com document.write().
-
-
+  Conteúdo detalhado desta aula:
+Sintaxe e criação de funções no JavaScript;
+Integrando funções criadas no JavaScript com o HTML que está sendo exibido na tela;
+Condicionando a execução (ou "chamada") de uma função a um clique em um botão na tela;
+Usando o JavaScript para acessar o que está sendo exibido na tela e pegar valores digitados pelo usuário com getElementById() e .value;
+Passar informações que as funções precisam para funcionar, através dos parâmetros;
+Utilizar o .endsWith() para verificar se um texto termina com determinados caracteres;
+Ver mais um exemplo de reatribuição de variável para "limpar" o texto do campo com "".
 */
